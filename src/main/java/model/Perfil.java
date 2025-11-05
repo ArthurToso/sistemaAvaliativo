59454f1c6d95d362ddf7497032b18acc50fa815e
ArthurToso <arthurtoso2003@gmail.com>
2025-11-05 15:28:37 -0300

    feat(pojo): Classe Perfil

--- src/main/java/model/Perfil.java
+++ src/main/java/model/Perfil.java
@@ -1,6 +1,66 @@
 package model;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "perfis")
 public class Perfil {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, unique = true)
     private String nome;
+
+    @OneToMany(mappedBy = "perfil")
+    private Set<Usuario> usuarios = new HashSet<>();
+
+    public Perfil() {
+    }
+
+    public Perfil(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Set<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Perfil perfil = (Perfil) o;
+        return Objects.equals(id, perfil.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
